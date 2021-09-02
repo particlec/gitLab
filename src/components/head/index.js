045bc267da47be960524637ef5dd0eef15d98fda
DESKTOP-EIHFT39\particle <906266319@qq.com>
2021-09-02 17:14:22 +0800

    feat: 提交测试

--- src/components/head/index.js
+++ src/components/head/index.js
@@ -3,15 +3,15 @@ import "./index.css";
 function Head() {
   return (
     <div className={"head-content"} style={{ position: "relative" }}>
+      <div>
+        <img
+          className={"head-content-item-icon"}
+          src={require("../../icon/slp-icon.png").default}
+          alt={"l"}
+        />
+        <h className={"head-content-item-name"}>GitLab</h>
+      </div>
       <div></div>
-      <div></div>
-
-      <img
-        className={"head-content-item-icon"}
-        src={require("../../icon/slp-icon.png").default}
-        alt={"l"}
-      />
-      <h className={"head-content-item-name"}>GitLab</h>
 
       {/*<div style={{ position: "absolute", marginLeft: "100px" }}>测试</div>*/}
     </div>
